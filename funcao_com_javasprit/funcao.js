76ab57ab720e69022c9c11364b35a655517e713d
Gabriel Rodrigues Rolim <100931450+rolim265@users.noreply.github.com>
2023-08-01 12:43:04 -0300

    Add files via upload

---
+++ funcao_com_javasprit/funcao.js
@@ -0,0 +1,10 @@
+saldo = 500
+function dadodeacesso(){
+    numcartao = prompt("Digite o número do seu cartão")
+    senha = prompt("Digite  a senha: ")
+}
+function saque(valor){
+    saldo = 500
+    saldo = saldo - valor
+    return saldo
+}
